56205b6f028fbd761c59bd8428c2350c03421327
sriram <sriram.tech.ai@gmail.com>
2026-07-09 17:26:13 +0530

    configure django settings for production

--- mylist/settings.py
+++ mylist/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-m#_xub6dfl40dk0hb5q)#!&e6ilpxr(&(od9%@lpm6&sw5-zd7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["sriramDailyTasks.pythonanywhere.com",]
 
 
 # Application definition
